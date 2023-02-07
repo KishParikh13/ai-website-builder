7e53708f2a860f5244a61a9d534f5fba6151a339
KishParikh13 <kaparikh@mail.smu.edu>
2023-02-06 22:57:54 -0500

    Update navbar

--- src/app/Navbar.jsx
+++ src/app/Navbar.jsx
@@ -1,27 +1,25 @@
-
-
 function Navbar (props) {
     return (
-      <header class="" style={{backgroundColor: '#' + props.siteColor + ''}}>
-        <nav class="mx-auto max-w-7xl px-6 lg:px-8" aria-label="Top">
-          <div class="flex w-full items-center justify-between py-3 lg:border-none">
-            <div class="flex items-center">
+      <header className="" style={{backgroundColor: '#' + props.siteColor + ''}}>
+        <nav className="mx-auto max-w-7xl px-6 lg:px-8" aria-label="Top">
+          <div className="flex w-full items-center justify-between py-3 lg:border-none">
+            <div className="flex items-center">
               <a href="#">
-                <span class="sr-only">Your Company</span>
-                <img class="h-10 w-auto" src={props.siteLogo} alt="" />
+                <span className="sr-only">Your Company</span>
+                <img className="h-10 w-auto" src={props.siteLogo} alt="" />
               </a>
-              <div class="ml-10 hidden space-x-8">
-                <a href="#" class="text-base font-medium text-white hover:text-indigo-50">Solutions</a>
+              <div className="ml-10 hidden space-x-8">
+                <a href="#" className="text-base font-medium text-white hover:text-indigo-50">Solutions</a>
   
-                <a href="#" class="text-base font-medium text-white hover:text-indigo-50">Pricing</a>
+                <a href="#" className="text-base font-medium text-white hover:text-indigo-50">Pricing</a>
   
-                <a href="#" class="text-base font-medium text-white hover:text-indigo-50">Docs</a>
+                <a href="#" className="text-base font-medium text-white hover:text-indigo-50">Docs</a>
   
-                <a href="#" class="text-base font-medium text-white hover:text-indigo-50">Company</a>
+                <a href="#" className="text-base font-medium text-white hover:text-indigo-50">Company</a>
               </div>
             </div>
-            <div class="ml-10 space-x-4">
-              <a href="#" class="inline-block rounded-md border border-transparent bg-white py-2 px-4 text-base font-medium hover:bg-indigo-50">{props.cta}</a>
+            <div className="ml-10 space-x-4">
+              <a href="#" className="inline-block rounded-md border border-transparent bg-white py-2 px-4 text-base font-medium hover:bg-indigo-50">{props.cta}</a>
             </div>
           </div>
         </nav>
