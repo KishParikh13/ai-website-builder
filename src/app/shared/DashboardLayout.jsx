348b107cafba7542d8646fbf18a0c0eb4adf1a10
KishParikh13 <kaparikh@mail.smu.edu>
2023-02-08 16:32:29 -0500

    Change logout button

--- src/app/shared/DashboardLayout.jsx
+++ src/app/shared/DashboardLayout.jsx
@@ -28,10 +28,20 @@ function DashboardLayout(props) {
                                             <div className="ml-4 flex items-center md:ml-6">
                                                 <div className="relative ml-3">
                                                     <div>
-                                                        <button onClick={e => logoutUser()} type="button" className="flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
+                                                        <Button
+                                                            onClick={e => logoutUser()}
+                                                            variant="solid"
+                                                            type="button"
+                                                            color="slate"
+                                                            className={" bg-transparent hover:bg-white/10 px-2 py-1"}
+                                                        >
+                                                            Logout
+                                                        </Button>
+                                                            
+                                                        {/* <button onClick={e => logoutUser()} type="button" className="flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
                                                             <span className="sr-only">Open user menu</span>
                                                             <img className="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
-                                                        </button>
+                                                        </button> */}
                                                     </div>
                                                 </div>
                                             </div>
