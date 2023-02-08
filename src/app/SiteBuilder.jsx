2c621daa0a13b0cfbd35be6ffd844912e8354b9e
KishParikh13 <kaparikh@mail.smu.edu>
2023-02-07 23:51:54 -0500

    Build out 1:1 editing w/ site editor

--- src/app/SiteBuilder.jsx
+++ src/app/SiteBuilder.jsx
@@ -17,40 +17,118 @@ function SiteBuilder() {
 
     const [site, setSite] = useState({});
 
+    const [OwnerName, setOwnerName] = useState('');
+    const [SiteName, setSiteName] = useState('');
+    const [SiteCTA, setSiteCTA] = useState('');
+    const [SiteCTALink, setSiteCTALink] = useState('');
+    const [SiteCTAHeading, setSiteCTAHeading] = useState('');
+    const [SiteColor, setSiteColor] = useState('');
+    const [SiteHeroHeading, setSiteHeroHeading] = useState('');
+    const [SiteServices, setSiteServices] = useState('');
+    const [SiteHeroSubheading, setSiteHeroSubheading] = useState('');
+    const [SiteRecordID, setSiteRecordID] = useState('');
+    const [SiteImages, setSiteImages] = useState('');
+    const [SiteLogo, setSiteLogo] = useState('');
+
+    const cleanString = (str) => {
+        return str.replace(/(\r\n|\n|\r)/gm, "")
+    }
+
     // load site data from url
     useEffect(() => {
-        if (siteID.length > 5) {
-            base.getSiteByID(siteID)
-                .then(response => {
-                    setSite(response)
-                })
+        console.log(siteID)
+        if (siteID !== undefined) {
+            loadSite()
         } else {
             window.location.href = '/dashboard'
         }
     }, [siteID]);
 
+    const loadSite = () => {
+        base.getSiteByID(siteID)
+                .then(response => {
+                    setSite(response)
+                    let siteFields = response.fields
+                    setOwnerName(siteFields.OwnerName)
+                    setSiteName(siteFields.Name)
+                    setSiteCTA(siteFields.SiteCTA)
+                    setSiteCTAHeading(siteFields.SiteCTAHeading)
+                    setSiteColor(siteFields.SiteColor)
+                    setSiteServices(siteFields.SiteServices)
+                    setSiteHeroHeading(cleanString(siteFields.SiteHeroHeading))
+                    setSiteHeroSubheading(cleanString(siteFields.SiteHeroSubheading))
+                    setSiteRecordID(siteFields.SiteRecordID)
+                    setSiteImages(siteFields.SiteImages)
+                    setSiteLogo(siteFields.SiteLogo)
+                })
+    }
+
+    const saveSiteChanges = () => {
+        base.updateSiteByID(siteID, {
+            "Name": SiteName,
+            "SiteCTA": SiteCTA,
+            "SiteCTAHeading": SiteCTAHeading,
+            "SiteColor": SiteColor,
+            "SiteHeroHeading": SiteHeroHeading,
+            "SiteServices": SiteServices,
+            "SiteHeroSubheading": SiteHeroSubheading,
+            "SiteImages": SiteImages,
+            "SiteLogo": SiteLogo
+        })
+        .then (response => {
+            console.log(response)
+        })
+    }
+
     return (
         <div>
             { site.fields && <>
                 <DashboardLayout Title={`Edit ${site.fields.Name}`} Content={
-                    <div class="grid grid-cols-1 items-start gap-4 lg:grid-cols-3 lg:gap-8">
-                        <div class="grid grid-cols-1 gap-4">
+                    <div className="grid grid-cols-1 items-start gap-4 lg:grid-cols-3 lg:gap-8">
+                        <div className="grid grid-cols-1 gap-4">
                             <section aria-labelledby="section-2-title">
-                                <h2 class="sr-only" id="section-2-title">Section title</h2>
-                                <div class="overflow-hidden rounded-lg bg-white shadow">
-                                <div class="p-6">
-                                    <SiteEditor />
-                                </div>
+                                <h2 className="sr-only" id="section-2-title">Section title</h2>
+                                <div className="overflow-hidden rounded-lg bg-white shadow">
+                                    <div className="p-6">
+                                        <SiteEditor
+                                            saveChanges={saveSiteChanges}
+                                            OwnerName={OwnerName} setOwnerName={setOwnerName}
+                                            SiteName={SiteName} setSiteName={setSiteName}
+                                            SiteCTA={SiteCTA} setSiteCTA={setSiteCTA}
+                                            SiteCTALink={SiteCTALink} setSiteCTALink={setSiteCTALink}
+                                            SiteCTAHeading={SiteCTAHeading} setSiteCTAHeading={setSiteCTAHeading}
+                                            SiteColor={SiteColor} setSiteColor={setSiteColor}
+                                            SiteHeroHeading={SiteHeroHeading} setSiteHeroHeading={setSiteHeroHeading}
+                                            SiteHeroSubheading={SiteHeroSubheading} setSiteHeroSubheading={setSiteHeroSubheading}
+                                            SiteServices={SiteServices} setSiteServices={setSiteServices}
+                                            SiteRecordID={SiteRecordID} siteRecordID={setSiteRecordID}
+                                            SiteImages={SiteImages} setSiteImages={setSiteImages}
+                                            SiteLogo={SiteLogo} setSiteLogo={setSiteLogo}
+                                        />
+                                    </div>
                                 </div>
                             </section>
                         </div>
 
-                        <div class="grid grid-cols-1 gap-4 lg:col-span-2">
+                        <div className="grid grid-cols-1 gap-4 lg:col-span-2">
                             <section aria-labelledby="section-1-title">
-                                <h2 class="sr-only" id="section-1-title">Site Preview</h2>
-                                <div class="overflow-hidden rounded-lg bg-white shadow">
-                                    <div class="p-6">
-                                        <SitePreview site={site} />
+                                <h2 className="sr-only" id="section-1-title">Site Preview</h2>
+                                <div className="overflow-hidden rounded-lg bg-white shadow">
+                                    <div className="p-6">
+                                        <SitePreview
+                                            OwnerName={OwnerName}
+                                            SiteName={SiteName}
+                                            SiteCTA={SiteCTA}
+                                            SiteCTALink={SiteCTALink}
+                                            SiteCTAHeading={SiteCTAHeading}
+                                            SiteColor={SiteColor}
+                                            SiteHeroHeading={SiteHeroHeading}
+                                            SiteHeroSubheading={SiteHeroSubheading}
+                                            SiteServices={SiteServices}
+                                            SiteRecordID={SiteRecordID}
+                                            SiteImages={SiteImages}
+                                            SiteLogo={SiteLogo}
+                                        />
                                     </div>
                                 </div>
                             </section>
--- src/app/SiteEditor.jsx
+++ src/app/SiteEditor.jsx
@@ -5,42 +5,167 @@ import { useState, useEffect } from 'react';
 import { TextArea, TextField } from '../components/Fields';
 import { Button } from '../components/Button';
 
-function SiteEditor(site) {
+const tabs = [
+    {
+        name: 'Profile',
+        href: '#',
+        current: true,
+    },
+    {
+        name: 'Projects',
+        href: '#',
+        current: false,
+    },
+    {
+        name: 'Settings',
+        href: '#',
+        current: false,
+    }
+]
+
+function TabsMenu(props) {
     return (
-        <div>
-            <h2 className='font-bold text-lg mb-4'>Edit Site</h2>
+        <div class="flex rounded-md shadow-sm" role="group">
+            <button type="button" onClick={e => props.setSelectedTab(tabs[0].name)} class={(props.selectedTab === tabs[0].name ? "bg-blue-800 text-white" : "bg-white text-black") + " flex-grow px-4 py-2 text-sm font-medium border border-gray-200 rounded-l-lg"}>
+                {tabs[0].name}
+            </button>
+            {
+                tabs.slice(1, tabs.length-1).map((tab, index) => {
+                    return (
+                        <button key={index} type="button" onClick={e => props.setSelectedTab(tab.name)} class={(props.selectedTab === tab.name ? "bg-blue-800 text-white" : "bg-white text-black") + " flex-grow px-4 py-2 text-sm font-medium border border-gray-200"}>
+                            {tab.name}
+                        </button>
+                    )
+                })
+            }
+            <button type="button" onClick={e => props.setSelectedTab(tabs[2].name)} class={(props.selectedTab === tabs[2].name ? "bg-blue-800 text-white" : "bg-white text-black") + " flex-grow px-4 py-2 text-sm font-medium border border-gray-200 rounded-r-md"}>
+                {tabs[tabs.length-1].name}
+            </button>
+        </div>
+    )
+}
+
+function ProfileFields(props) {
+    return (
+        <></>
+    )
+}
+
+
+function SiteEditor(props) {
+
+    const [selectedTab, setSelectedTab] = useState('Profile')
+    const [saving, setSaving] = useState(false)
+
+    return (
+        <div className='flex flex-col gap-4'>
+
+            <TabsMenu selectedTab={selectedTab} setSelectedTab={setSelectedTab} />
+            <h2 className='font-bold text-lg'>{selectedTab}</h2>
+
             <form className="grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-2">
-                <TextField
-                    className="col-span-full"
-                    type="text"
-                    label="Describe yourself and your skillset"
-                    id="self_description"
-                    name="self_description"
-                    required
-                />
-                <TextArea
-                    className="col-span-full"
-                    label="Describe yourself and your skillset"
-                    id="self_description"
-                    name="self_description"
-                    required
-                />
-                <TextArea
-                    className="col-span-full"
-                    label="What do you want people on your site to do?"
-                    id="site_goal"
-                    name="site_goal"
-                    required
-                />
+                {
+                    selectedTab === 'Profile' && <>
+                        <TextField
+                            className="col-span-full"
+                            type="text"
+                            label="Name of site"
+                            id="site_name"
+                            name="site_name"
+                            required
+                            value={props.SiteName}
+                            onChange={e => props.setSiteName(e.target.value)}
+                        />
+                        <TextArea
+                            className="col-span-full"
+                            label="Hero Heading"
+                            id="site_hero_heading"
+                            name="site_hero_heading"
+                            required
+                            value={props.SiteHeroHeading}
+                            onChange={e => props.setSiteHeroHeading(e.target.value)}
+                        />
+                        <TextArea
+                            className="col-span-full"
+                            label="Hero Subheading"
+                            id="site_hero_subheading"
+                            name="site_hero_subheading"
+                            required
+                            value={props.SiteHeroSubheading}
+                            onChange={e => props.setSiteHeroSubheading(e.target.value)}
+                        />
+                        {
+                            false && props.SiteServices && props.SiteServices.split('\n').filter(service => service.length > 0).map((service, index) => {
+                                return (
+                                    <TextField
+                                        key={index}
+                                        className="col-span-full"
+                                        type="text"
+                                        label={`Service ${index+1}`}
+                                        id={`site_service_${index+1}`}
+                                        name={`site_service_${index+1}`}
+                                        required
+                                        value={service}
+                                        onChange={e => {
+                                            let services = props.SiteServices.split('\n');
+                                            services[index] = e.target.value;
+                                            props.setSiteServices(services.join('\n'));
+                                        }}
+                                    />
+                                )
+                            })
+                        }
+                    </>
+                }
+                {
+                    selectedTab === 'Settings' && <>
+                        <TextField
+                            className="col-span-full"
+                            type="text"
+                            label="Call to action"
+                            id="site_cta"
+                            name="site_cta"
+                            required
+                            value={props.SiteCTA}
+                            onChange={e => props.setSiteCTA(e.target.value)}
+                        />
+                        <TextField
+                            className="col-span-full"
+                            type="text"
+                            label="CTA Link"
+                            id="site_cta_link"
+                            name="site_cta_link"
+                            required
+                            value={props.SiteCTALink}
+                            onChange={e => props.setSiteCTALink(e.target.value)}
+                        />
+                    </>
+                }
+                
                 <div className="col-span-full">
                     <Button
                         type="button"
                         variant="solid"
-                        color="slate"
+                        color= { saving ? "green" : "slate" }
                         className="w-full"
+                        onClick={e => {
+                            setSaving(true)
+                            props.saveChanges()
+                            setTimeout(() => setSaving(false), 2000)
+                        }}
                     >
                         <span>
-                            Save changes <span aria-hidden="true">&rarr;</span>
+                            {
+                                saving ? <div className='flex items-center gap-2'>
+                                    Saving...
+                                    <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                        <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                                        <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v8H4z"></path>
+                                    </svg>
+                                </div>
+                                :
+                                <>Save changes <span aria-hidden="true">&uarr;</span></>
+                            }
                         </span>
                     </Button>
                 </div>
