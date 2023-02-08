20858427c6c563fc30269125e24fdbd8b36eac3d
KishParikh13 <kaparikh@mail.smu.edu>
2023-02-08 00:12:53 -0500

    add cta and option to hide nav to dashboard layout

--- src/app/shared/DashboardLayout.jsx
+++ src/app/shared/DashboardLayout.jsx
@@ -1,3 +1,4 @@
+import { Button } from "../../components/Button";
 import Logo from "../../components/Logo";
 
 function DashboardLayout(props) {
@@ -11,35 +12,53 @@ function DashboardLayout(props) {
         <div>
             <div className="min-h-full">
                 <div className="bg-gray-800 pb-32">
-                    <nav className="bg-gray-800">
-                        <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                            <div className="border-b border-gray-700">
-                                <div className="flex h-16 items-center justify-between px-4 sm:px-0">
-                                    <div className="flex items-center">
-                                        <a href="/dashboard" className="flex-shrink-0">
-                                            <Logo fillColor="white" />
-                                        </a>
-                                    </div>
-                                    <div className="block ">
-                                        <div className="ml-4 flex items-center md:ml-6">
-                                            <div className="relative ml-3">
-                                                <div>
-                                                    <button onClick={e => logoutUser()} type="button" className="flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-                                                        <span className="sr-only">Open user menu</span>
-                                                        <img className="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
-                                                    </button>
+
+                    {
+                        !props.hideNav &&
+                        <nav className="bg-gray-800">
+                            <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                                <div className="border-b border-gray-700">
+                                    <div className="flex h-16 items-center justify-between px-4 sm:px-0">
+                                        <div className="flex items-center">
+                                            <a href="/dashboard" className="flex-shrink-0">
+                                                <Logo fillColor="white" />
+                                            </a>
+                                        </div>
+                                        <div className="block ">
+                                            <div className="ml-4 flex items-center md:ml-6">
+                                                <div className="relative ml-3">
+                                                    <div>
+                                                        <button onClick={e => logoutUser()} type="button" className="flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
+                                                            <span className="sr-only">Open user menu</span>
+                                                            <img className="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+                                                        </button>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    </nav>
+                        </nav>
+                    }
 
                     <header className="py-10">
-                        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-                            <h1 className="text-3xl font-bold tracking-tight text-white">{props.Title}</h1>
+                        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8  flex justify-between">
+                            <div className="flex items-center gap-4">
+                                {
+                                props.hideNav &&
+                                <a href="/dashboard" className="text-white text-2xl">
+                                    &larr;
+                                </a>
+                                }
+                                <h1 className="text-3xl font-bold tracking-tight text-white">{props.Title}</h1>
+                            </div>
+                            {
+                                props.cta &&
+                                <Button href={props.cta.link} text={props.buttonText}  color={props.cta.color}>
+                                    {props.cta.text}
+                                </Button>
+                            }
                         </div>
                     </header>
                 </div>
